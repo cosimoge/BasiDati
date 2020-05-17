77e4d4cf09025f5576c897c131875d296cb6e01c
cosimoge <64590780+cosimoge@users.noreply.github.com>
2020-05-17 11:57:30 +0200

    Add files via upload

--- Studenti.sql
+++ Studenti.sql
@@ -1,14 +1,16 @@
+drop database studenti;
 /*----------------Creazione e popolamento database--------------------*/
 create database if not exists studenti;
 use studenti;
 
-CREATE TABLE IF NOT EXISTS studente (
+create table if not exists studente(
     matricolaStudente INT PRIMARY KEY,
     corsoLaurea VARCHAR(50) NOT NULL,
     nome VARCHAR(30),
     cognome VARCHAR(30),
     dataNascita DATE,
     codiceFiscale VARCHAR(20) UNIQUE,
+    esamiSostenuti int,
     foto BLOB DEFAULT NULL
 );
 
@@ -17,6 +19,7 @@ matricola int primary key,
 dipartimento varchar(50),
 nome varchar(30),
 cognome varchar(30),
+eta int,
 dataNascita date,
 codiceFiscale varchar(20) unique,
 foto blob default null
@@ -46,7 +49,9 @@ descrizione varchar(80)
 
 create table if not exists dipartimento(
 codiceDipartimento varchar(10) primary key,
-nomeDipartimento varchar(30) unique
+nomeDipartimento varchar(30) unique,
+indirizzo varchar(40),
+citta varchar(20)
 );
 
 create table if not exists sede(
@@ -66,44 +71,44 @@ primary key(codiceSede,codiceDipartimento)
 
 
 
-insert into studente(matricolaStudente,corsoLaurea,nome,cognome,dataNascita,codiceFiscale,foto)
-		values('66900','ICD','Cosimo','Gennaro', '1999-10-20', 'GNNCSM99R20L049W', NULL);
-insert into studente(matricolaStudente,corsoLaurea,nome,cognome,dataNascita,codiceFiscale,foto)
-		values('66720','ICD','Simone','Marchitelli','1999-06-21','MRCSMN99H21S067L','');
+insert into studente(matricolaStudente,corsoLaurea,nome,cognome,dataNascita,codiceFiscale,esamiSostenuti,foto)
+		values('123456','ICD','Cosimo','Gennaro', '1999-10-20', 'GNNCSM99R20L049W','12', NULL);
+insert into studente(matricolaStudente,corsoLaurea,nome,cognome,dataNascita,codiceFiscale,esamiSostenuti,foto)
+		values('66720','ICD','Simone','Marchitelli','1999-06-21','MRCSMN99H21S067L','4','');
         
-insert into docente(matricola,dipartimento,nome,cognome,dataNascita,codiceFiscale,foto)
-		values('23400','Uniba','Riley','Reyd','1988-03-22','RYDRED43G27453A','');
+insert into docente(matricola,dipartimento,nome,cognome,dataNascita,eta,codiceFiscale,foto)
+		values('23400','Uniba','Riley','Reyd','1988-03-22','65','RYDRED43G27453A','');
 insert into docente(matricola,dipartimento,nome,cognome,dataNascita,codiceFiscale,foto)	
-		values('69123','Unisalerno','Abella','Denger','1975-11-12','ABEDEN32T23546U','');
+		values('69123','Unisa','Abella','Denger','1975-11-12','24','ABEDEN32T23546U','');
         
 insert into modulo(codice,nome,descrizione,CFU)
-		values('34420','BDD','Basi di dati','9');
+		values('34420','BDD','Basi di dati','6');
 insert into modulo(codice,nome,descrizione,CFU)
-		values('57490','LDP','linguaggi di programmazione','9');
+		values('57490','LDP','linguaggi di programmazione','12');
         
 insert into esame(matricolaStudente,codiceModulo,matricolaDocente,dataEsame,voto)
-        values('66720','57490','69123','2020-04-08','27');
+        values('123456','57490','69123','2020-04-08','27');
 insert into esame(matricolaStudente,codiceModulo,matricolaDocente,dataEsame,voto)
-        values('66900','34420','23400','2019-08-14','23');
+        values('66720','34420','23400','2019-08-14','23');
         
 insert into corsoLaurea(codiceCorso,nomeCorsoLaurea,descrizione)
         values('48723','PSI','Professioni sanitarie infermieristica');
 insert into corsoLaurea(codiceCorso,nomeCorsoLaurea,descrizione)
         values('43590','ICD','Informatica e comuncazione digitale');
 
-insert into dipartimento(codiceDipartimento,nomeDipartimento)
-        values('3408','Uniba');
-insert into dipartimento(codiceDipartimento,nomeDipartimento)
-        values('5532','Unisalerno');
+insert into dipartimento(codiceDipartimento,nomeDipartimento,indirizzo,citta)
+        values('3408','Uniba','via Martiri Otranto','Galatone');
+insert into dipartimento(codiceDipartimento,nomeDipartimento,indirizzo,citta)
+        values('5532','Unisalerno',"Via Toselli","Torricella");
         
 insert into sede(codiceSede,indirizzo,citta)
-        values('7698','via Alcide de Gasperi','Taranto');
+        values('INFO','via Alcide de Gasperi','Taranto');
 insert into sede(codiceSede,indirizzo,citta)
-        values('5634','Via Garibaldi','Salerno');
+        values('MEC','Via Garibaldi','Salerno');
         
 insert into sedeDipartimento(codiceSede,codiceDipartimento,note)
-        values('5634','5532','andratuttobene');
+        values('INFO','5532','andratuttobene');
 insert into sedeDipartimento(codiceSede,codiceDipartimento,note)
-        values('7698','3408','restateacasa');
+        values('MEC','3408','restateacasa');
 
 /*----------------Fine primo database--------------------*/
